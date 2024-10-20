3825bd865406f06d96b60815c4eb958fff07b7fe
Aarush Kumbhakern <aarushbeleren11@gmail.com>
2024-10-20 17:24:44 +0530

    Update HashTable.cpp

--- HashTable.cpp
+++ HashTable.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
 #include <optional>
+#include <math.h>
 
 typedef int key_t;
+
+// prime checker helper function
+bool isPrime(int x) {
+	for (size_t i = 2; i <= (int) std::sqrt(x); i++) {
+		if (x % i == 0) return false;
+	}
+
+	return true;	
+}
+
+// next prime helper function
+int nextPrime(int x) {
+	while (!isPrime(x)) x++;
+	return x;
+}
 
 // hashtable class
 class HashTable {
 	private:
 		int size;
+		float load = 0.0;
+		float alpha = 0.8;
 
 		std::optional<key_t>* table;
 
@@ -14,38 +32,57 @@ class HashTable {
 		int hash(key_t key) {
 			return key % size;
 		}
+
+		// resize table
+		void resize() {
+			HashTable ht(nextPrime(size * 2));
+
+			// re-insert all existing keys into new table
+			for (size_t i = 0; i < size; ++i) {
+				if (table[i].has_value()) ht.insert(table[i].value());
+			}
+
+			*this = std::move(ht); // swap data
+		}
 		
 	public:
 		// constructor
 		HashTable(int size) : size(size) {
 			table = new std::optional<key_t>[size]; // allocate memory for the table
-			for (int i = 0; i < size; ++i) table[i] = std::nullopt; // mark all slots as empty
+			for (size_t i = 0; i < size; ++i) table[i] = std::nullopt; // mark all slots as empty
 		}
 
 		// copy constructor
 		HashTable(const HashTable& other) : size(other.size) {
+			load = other.load;
+
 			table = new std::optional<key_t>[size];
-			for (int i = 0; i < size; ++i) table[i] = other.table[i];
+			for (size_t i = 0; i < size; ++i) table[i] = other.table[i];
 		}
 
-		// copy assignment operator
+		// copy assignment constructor
 		HashTable& operator=(const HashTable& other) {
-			if (this != &other) {
-				delete[] table;
-				size = other.size;
-				table = new std::optional<key_t>[size];
-				for (int i = 0; i < size; ++i) table[i] = other.table[i];
-			}
+			delete[] table;
+
+			size = other.size;
+			load = other.load;
+
+			table = new std::optional<key_t>[size];
+			for (size_t i = 0; i < size; ++i) table[i] = other.table[i];
 
 			return *this;
 		}
 
+		float getLoad() {
+			return load;
+		}
+
 		// insert
 		void insert(key_t key) {
-			int index = hash(key);
+			size_t index = hash(key);
 
 			// quadratic probing
-			for (int i = 0; i < size; i++) {
+			for (size_t i = 0; i < size; i++) {
 				int new_index = (index + i * i) % size;
 
 				// duplicate keys
@@ -57,38 +94,40 @@ class HashTable {
 				// empty slot
 				if (!table[new_index].has_value()) {
 					table[new_index] = key;
+					
+					load += (float) 1 / size;
+					if (load > alpha) resize();
+
 					return;
 				}
 			}
-
-			// printf("Max probing limit reached!\n");
 		}
 
 		// delete
 		void remove(key_t key) {
-			int index = hash(key);
+			size_t index = hash(key);
 
 			// quadratic probing
-			for (int i = 0; i < size; i++)	{
+			for (size_t i = 0; i < size; i++)	{
 				int new_index = (index + i * i) % size;
 
 				// key found
 				if (table[new_index] == key) {
 					table[new_index] = std::nullopt;  // mark slot as empty
+					load -= (float) 1 / size;
 					return;
 				}
 			}
 
-			// printf("Max probing limit reached!\n");
 			printf("Element not found\n");
 		}
 
 		// search
 		int search(key_t key) {
-			int index = hash(key);
+			size_t index = hash(key);
 
 			// quadratic probing
-			for (int i = 0; i < size; i++)	{
+			for (size_t i = 0; i < size; i++)	{
 				int new_index = (index + i * i) % size;
 
 				// key found
@@ -97,13 +136,12 @@ class HashTable {
 				}
 			}
 			
-			// printf("Max probing limit reached!\n");
 			return -1;
 		}
 
 		// print table
 		void printTable() {			
-			for (int i = 0; i < size; i++) {
+			for (size_t i = 0; i < size; i++) {
 				if (table[i].has_value()) printf("%d ", table[i]);
 				else printf("- ");
 			}
